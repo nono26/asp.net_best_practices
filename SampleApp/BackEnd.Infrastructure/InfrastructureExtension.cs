e332722b56d832a9b6f1f308b280e90fa421c8c8
Arnaud <atlam@hotmail.fr>
2024-05-29 15:06:00 +0000

    correction

--- SampleApp/BackEnd.Infrastructure/InfrastructureExtension.cs
+++ SampleApp/BackEnd.Infrastructure/InfrastructureExtension.cs
@@ -8,7 +8,8 @@ public static class InfrastructureExtension
 {
     public static IServiceCollection AddInfrastructure(this IServiceCollection services)
     {
-        services.AddSingleton(typeof(ILogger<>), typeof(ConsoleLogger<>));
+        services.AddSingleton(typeof(ILogger<>), typeof(ConsoleLogger<>));//Register the ConsoleLogger as ILogger as a singleton because it's a simple implementation of a logger
+        services.AddScoped<IReadWeatherForecastGateway, ReadWeatherForecastGateway>(); //Register the ReadWeatherForecastGateway as IReadWeatherForecastGateway as a scoped service because it might have a state
         return services;
     }
 }
